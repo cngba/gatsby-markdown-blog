3299b3a10b62fd39e1951727e435b9516c069987
CongNguyenBa <congphuc4716@gmail.com>
2024-02-14 18:05:05 +0700

    gogogo

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,28 @@
 import * as React from "react"
 import Layout from "../components/layout"
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDscJMW2fdRiua5VJgZINokmo1C0WHzSiM",
+  authDomain: "flowing-thoughts-32988.firebaseapp.com",
+  projectId: "flowing-thoughts-32988",
+  storageBucket: "flowing-thoughts-32988.appspot.com",
+  messagingSenderId: "800339923760",
+  appId: "1:800339923760:web:72737770f092280e47aef5",
+  measurementId: "G-9NP7MBWMYF"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+w
 const HomePage = () => {
   
   return (
