00f5c7a399d9d9c7bdc78f9b7a1ec6d1604a87a5
CongNguyenBa <congphuc4716@gmail.com>
2024-02-14 22:30:11 +0700

    Delete redundant requirements x2

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,6 @@
 import * as React from "react"
 import Layout from "../components/layout"
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-
 const HomePage = () => {
   
   return (
